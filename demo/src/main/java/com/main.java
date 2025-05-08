ae6fbae8c3b477a7ff5dbea25a6d5b16421cf7cd
Toma <100084874+Tomajashi@users.noreply.github.com>
2025-05-08 11:44:40 +0200

    a

---
+++ demo/src/main/java/com/main.java
@@ -0,0 +1,9 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class main {
+    public static void main(String[] args) {
+        SpringBootApplication.run(main.class, args);
+}
