39f7952f674c4800b7bc81212a8d06aaa7436e2f
Tomajashi <tomajashi5@gmail.com>
2025-05-12 19:32:10 +0200

    some TODOS add

--- demo/src/main/java/com/resources_calulator.java
+++ demo/src/main/java/com/resources_calulator.java
@@ -20,6 +20,8 @@ public class resources_calulator {
 
 
 //TODO: coins sollen wahrend des spiels hoch gezählt werden.
+//TODO: strom logik muss noch implementiert werden (basis 10 kwh. konsum von bahn 5kw und konsum von bahnhofer 2kw. konsum muss net die kapazität überschreiten)
+//TODO: manpower muss auch noch implementiert werden (jeder bahn braucht 3 manpower und jeder bahnhof 5 manpower. manpower konsum muss auch nicht die kapazität überschreiten)
     }
 
 
