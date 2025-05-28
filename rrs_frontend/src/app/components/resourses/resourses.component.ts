8d4b42eb0c848d0b0244bfa5907fb929896e4fc0
emily2036 <110234858+emily2036@users.noreply.github.com>
2025-05-28 13:27:25 +0200

    erste resourcen können ausgegeben werden

--- rrs_frontend/src/app/components/resourses/resourses.component.ts
+++ rrs_frontend/src/app/components/resourses/resourses.component.ts
@@ -11,13 +11,17 @@ import { CommonModule } from '@angular/common';
 })
 export class ResoursesComponent implements OnInit {
   displayedValue: number = 0; // Display value ist die ausgegebene wert auf den bild schirm
-  
+  coinZahl: number = 0; // Anzahl der Coins
+
   constructor(private apiService: APISService) {
     
   }
   ngOnInit(): void {
     this.apiService.getTotalTrains().subscribe((totalTrains) => {
-      this.displayedValue = totalTrains; //TODO: make displayvalue and trainsinfo same type
+      this.displayedValue = totalTrains; 
+    });
+    this.apiService.getDBCoins().subscribe((coins) => { //holt DB coins vom backend service
+      this.coinZahl = coins; 
     });
   }
 
--- rrs_frontend/src/app/services/apis.service.ts
+++ rrs_frontend/src/app/services/apis.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TrainsInfo } from '../models/trains-info.model';
+import { Resources } from '../models/resources.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -19,11 +20,17 @@ export class APISService {
 
   constructor(private http: HttpClient) {
     http.get<TrainsInfo>(`localhost:8080/getTrainInfo?playerId=${this.playerId}`).subscribe((trainInfo: TrainsInfo) => console.log(trainInfo))//TODO:
+    http.get<Resources>(`localhost:8080/getResources?playerId=${this.playerId}`).subscribe((resources: Resources) => console.log(resources))//TODO:
   }
 
   getTotalTrains(): Observable<number> {
   return this.http
     .get<TrainsInfo>(`${this.apiUrl}/getTrainInfo?playerId=${this.playerId}`).pipe(map((info: TrainsInfo) => info.total_trains));
+  }
+
+  getDBCoins(): Observable<number> {
+  return this.http
+    .get<Resources>(`${this.apiUrl}/getResources?playerId=${this.playerId}`).pipe(map((resources: Resources) => resources.DB_coin));
   }
 }
 
