8f8bda63e591a77d2347173e5da36468cf19dec3
emily2036 <110234858+emily2036@users.noreply.github.com>
2025-06-05 12:28:26 +0200

    anfang store

--- rrs_frontend/src/app/services/apis.service.ts
+++ rrs_frontend/src/app/services/apis.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { TrainsInfo } from '../models/trains-info.model';
+import { TrainsInfo } from '../models/trainsInfo.model';
 import { Resources } from '../models/resources.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
