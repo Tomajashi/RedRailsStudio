625db51038d85dab4e2512cfb50bdfb4dcb5075e
emily2036 <110234858+emily2036@users.noreply.github.com>
2025-06-10 11:43:18 +0200

    counter?

--- rrs_frontend/src/app/app.component.ts
+++ rrs_frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { MapComponent } from './components/map/map.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { ResoursesComponent } from './components/resourses/resourses.component';
 import { APISService } from './services/apis.service';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MapComponent, ResoursesComponent, SettingsComponent],  // Keep only MapComponent
+  imports: [MapComponent, ResoursesComponent],  // Keep only MapComponent
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
