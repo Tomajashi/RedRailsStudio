796a9174f5dd68bca6832e77c0b1319f5004a641
Toma <100084874+Tomajashi@users.noreply.github.com>
2025-06-12 12:15:41 +0200

    CORS

--- src/main/java/de/gts/redrail/RedRailIdleGameApplication.java
+++ src/main/java/de/gts/redrail/RedRailIdleGameApplication.java
@@ -2,12 +2,21 @@ package de.gts.redrail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class RedRailIdleGameApplication {
+public class RedRailIdleGameApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedRailIdleGameApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200") // or your frontend URL
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 	}
 
 }
